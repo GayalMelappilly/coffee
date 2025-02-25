07782f0b37b3f87cabda17c56deeb74c81213f8a
Gayal Melappilly <gayalsunil@gmail.com>
2025-02-25 22:08:46 +0530

    deploy(local-developement: fix): eslint fix #3

--- cup/app/profile/[username]/page.tsx
+++ cup/app/profile/[username]/page.tsx
@@ -1,15 +1,15 @@
 "use client"
 
-import SidePanel from '@/app/components/Home/SidePanel'
-import { useParams } from 'next/navigation'
+import SidePanel from '@/app/components/Home/SidePanel';
+import { useParams } from 'next/navigation';
 import React, { useState } from 'react'
 
-type Props = {}
+// type Props = {}
 
-const page = (props: Props) => {
+const Page = () => {
 
     const params = useParams();
-    const username = params?.id as string;
+    const username = params?.username as string;
 
     const [open, setOpen] = useState(false)
 
@@ -19,11 +19,11 @@ const page = (props: Props) => {
                 <div className="hidden sm:block sm:w-2/12">
                     <SidePanel open={open} setOpen={setOpen} />
                 </div>
-                <h1 className='mt-12 ml-20'>USERNAME : {username}</h1>
+                <h1 className='mt-12 ml-20 text-4xl'>USERNAME  : {username}</h1>
             </div>
             {/* <Discussions /> */}
         </>
     )
 }
 
-export default page
+export default Page
